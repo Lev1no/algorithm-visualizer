5b14d09e746c2e16850b664b3f31600d7e8b33a9
Lev1no <leevi.alanen1@gmail.com>
2024-12-13 16:26:40 +0200

    adding base for the webapp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Sorting Visualizer</h1>
+    </div>
+  );
+}
+
+export default App;
