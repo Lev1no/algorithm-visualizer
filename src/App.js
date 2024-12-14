3078b9b95fabf6c821930e365364eda543f163bf
Lev1no <leevi.alanen1@gmail.com>
2024-12-14 11:50:07 +0200

    adding untracked files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./styles/App.css";
-import "./styles/Navbar.css";
-import Navbar from "./components/Navbar";
 import SortingVisualizer from "./components/SortingVisualizer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <SortingVisualizer />
     </div>
   );
